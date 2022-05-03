f2c5ea67ccfc6251c73a6e5618d8d975c6e75ab3
nmusi <n.musinov@gmail.com>
2022-05-03 20:56:54 +0300

    connect to yandex storage. add method to get all files from cloud.

--- src/main/java/ru/geekbrains/videoservice/constants/Const.java
+++ src/main/java/ru/geekbrains/videoservice/constants/Const.java
@@ -5,8 +5,8 @@ package ru.geekbrains.videoservice.constants;
  * 30.04.2022
  */
 public class Const {
-    public static final String ACCESS_KEY = "YCAJE4YFa6e1AERI4IPyO1bmu";
-    public static final String SECRET_KEY = "YCN92Mm0PR8euOwbLvJxZ6aMQS6uGRj_EuiXfMb9";
+    public static final String ACCESS_KEY = "YCAJEMwFoI22gEPhxWrnB3O-N";
+    public static final String SECRET_KEY = "YCNgEBcZS_g6PWrhWhI2L1bxPfPmKEyx4b_zWQD9";
     public static final String BUCKET = "shorty";
     public static final String SERVER_ENDPOINT = "storage.yandexcloud.net";
     public static final String REGION = "ru-central1";
--- src/main/java/ru/geekbrains/videoservice/services/AmazonService.java
+++ src/main/java/ru/geekbrains/videoservice/services/AmazonService.java
@@ -1,9 +1,15 @@
 package ru.geekbrains.videoservice.services;
 
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.geekbrains.videoservice.Config.AmazonConfig;
 import ru.geekbrains.videoservice.constants.Const;
 
 import java.util.ArrayList;
@@ -14,14 +20,26 @@ import java.util.List;
  * 30.04.2022
  */
 @Service
-@AllArgsConstructor
+
 public class AmazonService {
 
-    private final AmazonConfig amazonConfig;
+    AWSCredentials credentials = new BasicAWSCredentials(
+            Const.ACCESS_KEY,
+            Const.SECRET_KEY
+    );
+
+
+    AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                    Const.SERVER_ENDPOINT, Const.REGION)
+            )
+            .build();
+
 
     public List<String> getAllFiles() {
         List<String> filesNames = new ArrayList();
-        List<S3ObjectSummary> files = amazonConfig.getS3().listObjects(Const.BUCKET).getObjectSummaries();
+        List<S3ObjectSummary> files = s3.listObjects(Const.BUCKET).getObjectSummaries();
         for (S3ObjectSummary list : files) {
             filesNames.add("net/" + list.getKey());
         }
