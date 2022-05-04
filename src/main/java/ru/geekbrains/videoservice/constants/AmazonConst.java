0d5074152988cca48de77843a788ed67263a58e4
nmusi <n.musinov@gmail.com>
2022-05-04 21:56:24 +0300

    formatting code

--- src/main/java/ru/geekbrains/videoservice/constants/AmazonConst.java
+++ src/main/java/ru/geekbrains/videoservice/constants/AmazonConst.java
@@ -4,7 +4,7 @@ package ru.geekbrains.videoservice.constants;
  * @author Nick Musinov e-mail:n.musinov@gmail.com
  * 30.04.2022
  */
-public class Const {
+public class AmazonConst {
     public static final String ACCESS_KEY = "YCAJEMwFoI22gEPhxWrnB3O-N";
     public static final String SECRET_KEY = "YCNgEBcZS_g6PWrhWhI2L1bxPfPmKEyx4b_zWQD9";
     public static final String BUCKET = "shorty";
--- src/main/java/ru/geekbrains/videoservice/services/AmazonService.java
+++ src/main/java/ru/geekbrains/videoservice/services/AmazonService.java
@@ -8,9 +8,8 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.geekbrains.videoservice.constants.Const;
+import ru.geekbrains.videoservice.constants.AmazonConst;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,26 +19,25 @@ import java.util.List;
  * 30.04.2022
  */
 @Service
-
 public class AmazonService {
 
     AWSCredentials credentials = new BasicAWSCredentials(
-            Const.ACCESS_KEY,
-            Const.SECRET_KEY
+            AmazonConst.ACCESS_KEY,
+            AmazonConst.SECRET_KEY
     );
 
 
     AmazonS3 s3 = AmazonS3ClientBuilder.standard()
             .withCredentials(new AWSStaticCredentialsProvider(credentials))
             .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
-                    Const.SERVER_ENDPOINT, Const.REGION)
+                    AmazonConst.SERVER_ENDPOINT, AmazonConst.REGION)
             )
             .build();
 
 
     public List<String> getAllFiles() {
         List<String> filesNames = new ArrayList();
-        List<S3ObjectSummary> files = s3.listObjects(Const.BUCKET).getObjectSummaries();
+        List<S3ObjectSummary> files = s3.listObjects(AmazonConst.BUCKET).getObjectSummaries();
         for (S3ObjectSummary list : files) {
             filesNames.add("net/" + list.getKey());
         }
